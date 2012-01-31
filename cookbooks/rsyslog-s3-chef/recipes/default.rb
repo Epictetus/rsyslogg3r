01a15c0ed3ab1eafc3e99c6c4d2369e14c5cda50
Aaron Peckham <apeckham@local>
2012-01-31 14:10:50 -0800

    don't download s3cmd every time

--- cookbooks/rsyslog-s3-chef/recipes/default.rb
+++ cookbooks/rsyslog-s3-chef/recipes/default.rb
@@ -1,12 +1,14 @@
-remote_file "#{Chef::Config[:file_cache_path]}/s3cmd-1.0.0.tar.gz" do
-  source "http://sourceforge.net/projects/s3tools/files/s3cmd/1.0.0/s3cmd-1.0.0.tar.gz/download"
-end
-
 bash "install s3cmd" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOF
-    tar xvfz s3cmd-1.0.0.tar.gz
+    tar xfz s3cmd-1.0.0.tar.gz
+    cd s3cmd-1.0.0
     python setup.py install
   EOF
   creates "/usr/bin/s3cmd"
 end
+
+# cookbook_file "/etc/rsyslog.conf" do
+#   source "rsyslog"
+#   mode "600"
+# end
