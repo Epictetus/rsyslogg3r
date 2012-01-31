7dde5bf87e03d724a214de83d0e3ca753fa4fd25
Aaron Peckham <apeckham@local>
2012-01-31 13:52:49 -0800

    skip gem install if soloist is present

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
-sudo yum install -y rubygems ruby-devel make gcc
-sudo gem install soloist-0.9.4.gem --no-ri --no-rdoc
+sudo yum -C install -y rubygems ruby-devel make gcc
+command -v soloist || { sudo gem install soloist-0.9.4.gem --no-ri --no-rdoc; }
 soloist
